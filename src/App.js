e6b9adaa549fb89416f5da5d54dfd1e587ca2278
freya0608 <freya001@yeah.net>
2019-01-30 18:17:41 +0800

    solders

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import {Button,List} from 'antd-mobile';
+// import 'antd-mobile/dist/antd-mobile.css'
 
 class App extends Component {
     constructor(props) {
@@ -9,21 +11,69 @@ class App extends Component {
         }
     }
 
-    addSolieder = ()=>{
-        console.log('hello add solider');
-        this.setState({
-            solders:[]
-        });
 
-    };
     render() {
+        const boss = '李云龙';
         return (
             <div>
-                <h2>独立团</h2>
-            </div>
+                <h2>独立团{boss}</h2>
+                <Yiying1/>
 
+                <Yiying/>
+
+            </div>
         );
-  }
+    }
 }
+
+
+function Yiying1() {
+    return (
+        <div>函数和class是一样的</div> // =函数和class是一样的；
+    );
+}
+
+class Yiying extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            solders:['胡子','朱子','胖子']
+        }
+        this.addSolider = this.addSolider.bind(this);//第一种方法
+    }
+
+
+    addSolider = ()=>{ //第二种方法，箭头函数
+        console.log('hello add solider');
+        this.setState({
+            solders:[...this.state.solders,'新兵蛋子'+ Math.random()]
+        });
+    };
+    render() {
+        const boss = '张三';
+        return (
+            <div>
+                <Button type="primary" onClick={this.addSolider}>新兵入伍</Button>
+
+                <List renderHeader={()=>{ '士兵列表' }}>
+                    {
+
+                        this.state.solders.map((item,index)=>{
+                            return <List.Item key={index} className="">
+                                    {item}
+                                  </List.Item>
+                         })
+
+                    }
+                </List>
+
+                <h2>一营营长，{boss}</h2>
+                {/*<button onClick={()=>this.addSolider()}>新兵入伍</button>   第三种方法，箭头函数*/}
+
+            </div>
+        );
+    }
+}
+
 
 export default App;
