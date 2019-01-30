5417af2e82e82471ad10007dc9e495cac434f17c
freya0608 <freya001@yeah.net>
2019-01-30 13:58:04 +0800

    solders

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    constructor(props) {
+        super(props);
+        this.state = {
+            solders:['胡子','朱子','胖子']
+        }
+    }
+
+    addSolieder = ()=>{
+        console.log('hello add solider');
+        this.setState({
+            solders:[]
+        });
+
+    };
+    render() {
+        return (
+            <div>
+                <h2>独立团</h2>
+            </div>
+
+        );
   }
 }
 
